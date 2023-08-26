3f0484470c6531713a23b44a89a374b66e0edc09
Puneet Sharma <puneetsharma9@hotmail.com>
2023-08-26 12:42:40 +0530

    minor update

--- setup.php
+++ setup.php
@@ -4,7 +4,7 @@ use Puneetxp\CompilePhp\setup;
 
 require "./vendor/autoload.php";
 // require_once __DIR__ . '/setup/autosetup.php';
-
+$setup = new setup(__DIR__);
+$setup->config();
 //(new mysqltable())->migrate();
-(new setup(__DIR__))->config();
 // (new setup(__DIR__))->table_set()->php_set()->deno_set()->angular_set()->write();
